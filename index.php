d3822ae2540dc9a301cbb82c0841d6d90679d0b0
arpan29 <arpanzbq29@gmail.com>
2019-08-08 12:02:41 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
 <body>
     <h1>Hello Word</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque earum dolor doloremque error cumque neque aliquam dignissimos nemo consectetur laudantium molestias officia nostrum adipisci dicta consequuntur, tenetur odio eius vero.</p>
+    <ul>
+        <li>arpan</li>
+    </ul>
 </body>
 
 </html>
